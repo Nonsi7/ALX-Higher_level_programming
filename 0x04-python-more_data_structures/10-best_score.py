31aad726c62463ad170963f976e78fc354fcbc31
Nonsi7 <debockannaline@gmail.com>
2022-07-11 11:47:53 -0700

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -8,9 +8,9 @@ def best_score(a_dictionary):
         return None
 
     ret = list(a_dictionary.keys())[0
-            big = a_dictionary[ret]
-            for k, v in a_dictionary.items():
-                if v > big:
-                    big = v
-                    ret = k
-            return (ret)
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+    return (ret)
